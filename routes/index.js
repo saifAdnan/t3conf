16a7968113bf82a037be5169fde1cb9cb6cd07c4
Saif <saif.mowefak@gmail.com>
2014-07-01 14:21:53 +0300

    index

--- routes/index.js
+++ routes/index.js
@@ -382,6 +382,7 @@ module.exports = function (app, rooms, ami, confs) {
     });
 
     app.get('/action/confs', function (req, res) {
+        console.log(confs.length, 'confis length');
         for (var i = 0; i < confs.length; i++) {
             console.log(confs, confs.length, 'saif');
             Conferences.collection.find({name: confs[i]}).toArray(function (err, doc) {
