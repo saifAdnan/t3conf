442962de44f3a32c5bd9d640f8531cedaf513b13
Saif <saif.mowefak@gmail.com>
2014-07-01 13:57:49 +0300

    index

--- app.js
+++ app.js
@@ -159,13 +159,7 @@ ami.on('ami_data', function (data) {
          */
         if (!conferences[data.conference]) conferences[data.conference] = {};
 
-            Conferences.collection.find({name: data.conference}).toArray(function (err, doc) {
-                if (doc.length > 0) {
-                    console.log(doc[0], 'doc[0]');
-                    conferences[doc[0].name].name = doc[0].name;
-                    conferences[doc[0].name].sip = doc[0].sip;
-                }
-            });
+
     }
 });
 
