ed4af09fb490493d68eb8d7f3ba06c67b0172869
Saif <saif.mowefak@gmail.com>
2014-07-02 11:16:26 +0300

    static conferences

--- public/js/angular/controllers/mainController.js
+++ public/js/angular/controllers/mainController.js
@@ -78,7 +78,7 @@ function mainController($scope, $http, $location, watchService, sipService, $roo
     };
 
     $scope.generateSipNumber = function () {
-        $scope.conf_sip = Math.floor(Math.random() * 90000) + 1000;
+        $scope.conf_sip = Math.floor(Math.random() * 9000) + 1000;
     };
 
     sipService.sipHangUp();
