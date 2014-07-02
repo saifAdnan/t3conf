85aa6a6568d565142c7332d0d992fad86e29f65d
Saif <saif.mowefak@gmail.com>
2014-07-02 13:36:26 +0300

    static conferences

--- public/js/angular/controllers/mainController.js
+++ public/js/angular/controllers/mainController.js
@@ -90,7 +90,7 @@ function mainController($scope, $http, $location, watchService, sipService, $roo
         e.preventDefault();
         if (confirm("Are you sure to delete all records?")) {
             $http.post("/action/clearRecords");
-            $location.url("/");
+            window.location.reload();
             return false
         } else {
             return false;
@@ -101,7 +101,7 @@ function mainController($scope, $http, $location, watchService, sipService, $roo
         e.preventDefault();
         if (confirm("Are you sure to deletethis record?")) {
             $http.post("/action/clearRecord", {filename: filename});
-            $location.url("/");
+            window.location.reload();
             return false
         } else {
             return false;
