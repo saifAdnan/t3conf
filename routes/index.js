9439af10a0b9bc2dc3bd817708774315e21a7d87
Saif <saif.mowefak@gmail.com>
2014-07-02 12:08:52 +0300

    static conferences

--- routes/index.js
+++ routes/index.js
@@ -469,6 +469,7 @@ module.exports = function (app, rooms, ami, confs) {
     });
 
     app.post('/action/clearRecords', function (req, res) {
+        console.log(__dirname, 'loc');
         fs.readdir('../asterisk/monitor', function (err, files) {
             files.forEach(function(filename) {
                 fs.unlink(filename, function (err) {
