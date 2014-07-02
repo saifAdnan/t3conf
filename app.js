f2f04414ddac51cfec79ecd5e04566acb0de507f
Saif <saif.mowefak@gmail.com>
2014-07-02 11:26:15 +0300

    static conferences

--- app.js
+++ app.js
@@ -155,6 +155,7 @@ ami.on('ami_data', function (data) {
                 ) {
                 delete conferences[data.conference];
                 io.sockets.emit('user:join', conferences);
+                console.log(data.conference, typeof data.conferencem, 'typeof');
                 Conferences.collection.remove({sip: data.conference});
                 console.log('\n\nCONF DELETED', conferences);
             } else {
