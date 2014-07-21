cf5507063d3e598b436a4a2bc7261b3dc68051e5
Saif <saif.mowefak@gmail.com>
2014-07-21 10:57:05 +0300

    sip settings

--- public/js/angular/directives/navSuper.js
+++ public/js/angular/directives/navSuper.js
@@ -4,7 +4,8 @@ app.directive("navSuper", function () {
         replace: true,
         templateUrl: PATH + 'templates/nav.html',
         link: function (scope, element, attr) {
-            //
+            scope.sip = SIP;
+            scope.password = PASSWORD;
         }
     };
 });
